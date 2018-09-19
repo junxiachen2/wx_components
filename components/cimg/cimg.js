fad65d734ddf28b15818a625e590c8ee0e0e31e8
junxia chen <junxiachen@junxiadeMacBook-Pro.local>
2018-09-19 11:45:33 +0800

    fix: cimg&cell-group

--- components/cimg/cimg.js
+++ components/cimg/cimg.js
@@ -1,29 +1,64 @@
 Component({
   properties: {
-    // 图片url
     url: {
       type: String,
-      value: ''
+      value: '',
+      observer (newVal, oldVal) {
+        if (newVal && newVal !== oldVal) {
+          const { placehold } = this.data
+          this.setData({ ph: placehold })
+        }
+        let validUrl = ''
+        if (newVal && !newVal.match('null') && !newVal.match('undefined')) {
+          validUrl = newVal
+        }
+        this.setData({ validUrl })
+      }
     },
-    // 占位图url
     placehold: {
+      type: String,
+      value: '',
+      observer (newVal) {
+        if (newVal) {
+          this.setData({ ph: newVal })
+        }
+      }
+    },
+    errImg: {
       type: String,
       value: ''
     },
     mode: {
       type: String,
-      value: 'scaleToFill'
+      value: 'widthFix'
+    },
+    preview: {
+      type: Boolean,
+      value: false
     }
+  },
+  data: {
+    ph: '',
+    validUrl: ''
   },
   methods: {
     imgLoadedHandler (e) {
-      let { url, placehold } = this.data
-      placehold = url
-      this.setData({ placehold })
+      const { url } = this.data
+      this.setData({ ph: url })
       this.triggerEvent('imgloaded', e)
     },
     imgErrorHandler (e) {
+      const { errImg } = this.data
+      this.setData({ ph: errImg })
       this.triggerEvent('imgerror', e)
+    },
+    imgTapHandler () {
+      const { validUrl, preview } = this.data
+      if (validUrl && preview) {
+        wx.previewImage({
+          urls: [validUrl]
+        })
+      }
     }
   }
 })
